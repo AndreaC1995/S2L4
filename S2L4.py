650d54beccaf83f9e7d9035a089abd676264e7eb
AndreaC1995 <andrea.cardinali1995@gmail.com>
2024-11-14 16:41:41 +0100

    Add files via upload

---
+++ S2L4.py
@@ -0,0 +1,27 @@
+import math
+
+print("Scegli una figura geometrica per calcolare il perimetro:")
+print("1. Quadrato")
+print("2. Cerchio")
+print("3. Rettangolo")
+
+scelta = input("Inserisci il numero della tua scelta: ")
+
+if scelta == '1':
+    lato = float(input("Inserisci la lunghezza del lato del quadrato: "))
+    perimetro = lato * 4
+    print(f"Il perimetro del quadrato è: {perimetro}")
+
+elif scelta == '2':
+    raggio = float(input("Inserisci il raggio del cerchio: "))
+    circonferenza = 2 * math.pi * raggio
+    print(f"La circonferenza del cerchio è: {circonferenza}")
+
+elif scelta == '3':
+    base = float(input("Inserisci la lunghezza della base del rettangolo: "))
+    altezza = float(input("Inserisci l'altezza del rettangolo: "))
+    perimetro = 2 * base + 2 * altezza
+    print(f"Il perimetro del rettangolo è: {perimetro}")
+
+else:
+    print("Scelta non valida. Riprova.")
